c506d2957e575f146626f53ba2df9cc68b1e13aa
Jon Kobeaga <j.kobeaga@lookiero.com>
2020-11-03 17:52:02 +0100

    Color detection on LK images

--- color_detection_cron.sh
+++ color_detection_cron.sh
@@ -1,6 +1,5 @@
-PATH=/home/ubuntu/anaconda3/envs/amy/bin
-#PATH=/home/jkobe/anaconda3/envs/amy3/bin
-export PYTHONPATH=${PYTHONPATH}:/usr/share/lookiero/color_detection:/home/ubuntu/anaconda3/envs/amy/lib/python3.6/site-packages/ds-data-core/
+PATH=/home/jkobe/anaconda3/envs/amy3/bin
+export PYTHONPATH=${PYTHONPATH}:/usr/share/lookiero/color_detection:/home/ubuntu/anaconda3/envs/amy3/lib/python3.7/site-packages/ds-data-core/
 #export PYTHONPATH=${PYTHONPATH}:/home/jkobe/Lookiero/color_detection:/home/jkobe/anaconda3/envs/amy3/lib/python3.7/site-packages/ds-data-core/
 cd /usr/share/lookiero/color_detection/
 #cd /home/jkobe/Lookiero/color_detection/
