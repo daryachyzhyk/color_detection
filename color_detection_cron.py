fd271c78a15d6460e94a161de063b442d95e8ce1
Jon Kobeaga <j.kobeaga@lookiero.com>
2020-11-03 16:49:27 +0100

    Color detection on LK images

--- color_detection_cron.py
+++ color_detection_cron.py
@@ -2,8 +2,8 @@
 from color_detection_extcolors import ColorExtraction
 
 
-def color_detection_cron():
-    ce = ColorExtraction()
+def color_detection_cron(local=True):
+    ce = ColorExtraction(local=local)
     ce.get_LK_color()
     ce.get_LK_images_info()
 
